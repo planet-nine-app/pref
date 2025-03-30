ed79a0e806fdedaec013efbb70fa0c60428a06d0
zach-planet-nine <zach@planetnine.app>
2025-03-30 09:02:21 -0700

    fix

--- src/server/node/pref.js
+++ src/server/node/pref.js
@@ -42,7 +42,7 @@ const bootstrap = async () => {
       spellbooks
     };
 
-    if(!pref.fountUUID || !pref.bdoUUID || !spellbooks || spellbooks.length === 0) {
+    if(!pref.fountUUID || !pref.bdoUUID || !spellbooks) {
       throw new Error('bootstrap failed');
     }
 
